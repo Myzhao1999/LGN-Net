a900b7edabb83910ac00c6d14521618a833c33cf
Mengyang Zhao <59856188+Myzhao1999@users.noreply.github.com>
2022-11-15 23:52:52 +0800

    Update lgn_net.py

--- LGN-Net/model/lgn_net.py
+++ LGN-Net/model/lgn_net.py
@@ -1,2 +1,4 @@
 ### The complete code will be available after the paper is accepted.
-### If you are interested in our work, please contact us by email myzhao@fudan.edu.cn  
+### If you are interested in our work, please contact us by email to get the initial version code.
+
+### Email:myzhao@fudan.edu.cn 
